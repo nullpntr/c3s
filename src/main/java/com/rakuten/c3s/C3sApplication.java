fddae8fcbd86b2dec95197996f544c3e9e1280d0
Bhaskar <anil.bhaskar@rakuten.com>
2019-03-26 11:36:44 +0530

    first commit

---
+++ src/main/java/com/rakuten/c3s/C3sApplication.java
@@ -0,0 +1,13 @@
+package com.rakuten.c3s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C3sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C3sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakuten/c3s/C3sApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rakuten.c3s;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class C3sApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
